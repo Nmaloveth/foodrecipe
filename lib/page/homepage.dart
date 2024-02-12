141898acc3ee8ecdcdc349a9d211aad7dfb32d75
Loveth Anoliefo <anoliefoloveth1@gmail.com>
2024-02-12 07:00:04 +0100

    Created a titleinfosection file and modified my home page

--- lib/page/homepage.dart
+++ lib/page/homepage.dart
@@ -12,10 +12,10 @@ class MyHomePage extends StatefulWidget {
 class _HomePageState extends State<MyHomePage>
     with SingleTickerProviderStateMixin {
   late AnimationController _firstcontroller;
-  late Animation<double> _animation;
-  late Animation<double> _animation1;
-  late Animation<double> _animation2;
-  late Animation<double> _animation3;
+  late Animation<double> _firstanimation;
+  late Animation<double> _secondanimation;
+  late Animation<double> _thirdanimation;
+  late Animation<double> _fourthanimation;
 
   @override
   void initState() {
@@ -24,28 +24,28 @@ class _HomePageState extends State<MyHomePage>
       vsync: this,
       duration: const Duration(milliseconds: 1000), // Adjust duration as needed
     );
-    _animation = Tween<double>(begin: 0.0, end: 1.0).animate(
+    _firstanimation = Tween<double>(begin: 0.0, end: 1.0).animate(
       CurvedAnimation(
         parent: _firstcontroller,
         curve: Curves.bounceIn,
       ),
     );
 
-    _animation1 = Tween<double>(begin: 0.0, end: 1.0).animate(
+    _secondanimation = Tween<double>(begin: 0.0, end: 1.0).animate(
       CurvedAnimation(
         parent: _firstcontroller,
         curve: Curves.easeIn,
       ),
     );
 
-    _animation2 = Tween<double>(begin: 0.0, end: 1.0).animate(
+    _thirdanimation = Tween<double>(begin: 0.0, end: 1.0).animate(
       CurvedAnimation(
         parent: _firstcontroller,
         curve: Curves.easeInCirc,
       ),
     );
 
-    _animation3 = Tween<double>(begin: 0.0, end: 1.0).animate(
+    _fourthanimation = Tween<double>(begin: 0.0, end: 1.0).animate(
       CurvedAnimation(
         parent: _firstcontroller,
         curve: Curves.easeInQuad,
@@ -68,6 +68,56 @@ class _HomePageState extends State<MyHomePage>
 
     return Scaffold(
       appBar: const Appbar_(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 22.0, vertical: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              FadeTransition(
+                opacity: _firstanimation,
+                child: Text(
+                  ConstantVariables.titleText,
+                  style: textTheme.displayLarge,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              FadeTransition(
+                opacity: _firstanimation,
+                child: Video(backgroundImage: ConstantVariables.background),
+              ),
+              const SizedBox(
+                height: 14,
+              ),
+              FadeTransition(
+                opacity: _secondanimation,
+                child: Rating(
+                    rating: ConstantVariables.rating,
+                    reviews: ConstantVariables.reviews),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              FadeTransition(
+                opacity: _thirdanimation,
+                child: AuthorProfile(
+                    image: ConstantVariables.user,
+                    name: ConstantVariables.userName,
+                    location: ConstantVariables.userLocation),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              FadeTransition(
+                opacity: _fourthanimation,
+                child: const Ingredients(),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
